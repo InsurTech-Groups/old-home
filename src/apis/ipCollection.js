9e36b0a69b58b79b0ba6ab2528399b36c40bd21f
David <david@tangentdigitalagency.com>
2023-03-17 21:39:51 -0700

    fixed non first step issue

--- src/apis/ipCollection.js
+++ src/apis/ipCollection.js
@@ -2,20 +2,20 @@
 
 export const ipAddress = async (ip) => {
 
-  let apiKey = process.env.REACT_APP_IP_API_KEY;
-  let ipAddress = localStorage.getItem('ip')
+  // let apiKey = process.env.REACT_APP_IP_API_KEY;
+  // let ipAddress = localStorage.getItem('ip')
 
-  ('the ip is, ', ipAddress)
+  // ('the ip is, ', ipAddress)
 
-  const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}&ip_address=${ipAddress}`
+  // const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}&ip_address=${ipAddress}`
 
-  //make a fetch request
-  fetch(url)
+  // //make a fetch request
+  // fetch(url)
 
-    .then(response => {
-      response.json();
+  //   .then(response => {
+  //     response.json();
 
 
       
-    });
+  //   });
 }
